dc726cd3dc1a67e9b6593f8c3530a19622cfc53f
churakovra <churakov.main@gmail.com>
2025-01-28 20:36:14 +0300

    Разработка логики команды /add

--- handlers/add.py
+++ handlers/add.py
@@ -1,9 +1,32 @@
+import re
+
 from aiogram import Router
-from aiogram.filters import Command
+from aiogram.filters import Command, CommandObject
 from aiogram.types import Message
 
 router = Router()
+error_text = (
+    "Для добавления задачи используй /add <task> <date> команду\n"
+    "Пример: /add Сходить в магазин 24.10.2025"
+)
+
 
 @router.message(Command("add"))
-async def command_add(message: Message):
-    pass
+async def command_add(message: Message, command: CommandObject):
+    if not command.args:
+        await message.answer(error_text)
+        return
+
+    try:
+        pattern = r"(\d{2}\.\d{2}\.\d{4})"
+        match = re.search(pattern=pattern, string=command.args)
+        if match:
+            date = match.group(1)
+            remaining_text = command.args.replace(date, '').strip()
+        else:
+            raise ValueError
+    except ValueError:
+        await message.answer(error_text)
+        return
+    # TODO Добавление в БД
+    await message.answer(f"Задача добавлена: \"{remaining_text}\" до {date}")
