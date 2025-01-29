6d5ee42eee04dc47668ede993709e10dd98a411f
churakovra <churakov.main@gmail.com>
2025-01-29 23:33:09 +0300

    add save to db in add handler

--- handlers/add.py
+++ handlers/add.py
@@ -4,6 +4,8 @@ from aiogram import Router
 from aiogram.filters import Command, CommandObject
 from aiogram.types import Message
 
+from database.tasker_db import add_task
+
 router = Router()
 error_text = (
     "Для добавления задачи используй /add <task> <date> команду\n"
@@ -21,12 +23,21 @@ async def command_add(message: Message, command: CommandObject):
         pattern = r"(\d{2}\.\d{2}\.\d{4})"
         match = re.search(pattern=pattern, string=command.args)
         if match:
+
             date = match.group(1)
             remaining_text = command.args.replace(date, '').strip()
+
         else:
             raise ValueError
     except ValueError:
         await message.answer(error_text)
         return
-    # TODO Добавление в БД
+
+    task = {'task': str(remaining_text), 'date_to_do': date, 'user_id': message.from_user.id}
+    user = {
+        'user_id': task['user_id'],
+        'username': message.from_user.username,
+        'fullname': message.from_user.full_name or None
+    }
+    add_task(task, user)
     await message.answer(f"Задача добавлена: \"{remaining_text}\" до {date}")
