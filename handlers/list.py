a821ca556caa13d02547cf41996846fa24efb55d
churakovra <churakov.main@gmail.com>
2025-01-29 23:33:23 +0300

    make /list handler

--- handlers/list.py
+++ handlers/list.py
@@ -1,9 +1,10 @@
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
+from database.tasker_db import get_user_tasks
 
 router = Router()
 
 @router.message(Command("list"))
 async def command_list(message: Message):
-    pass
+    await message.answer(get_user_tasks(message.from_user.id))
